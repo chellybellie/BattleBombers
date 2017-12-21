eefbcb301b0dcc1e530a7421e70cfdf73d8c7ed3
gingerfarrar <ginger.farrar@students.aie.edu.au>
2017-12-21 12:48:08 -0800

    adding ginger

---
+++ Assets/Ginger/Ginger/SetupLocalPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SetupLocalPlayer : NetworkBehaviour
+{
+    [SyncVar]
+    public string pname = "1";
+	
+	void Start ()
+    {
+		
+	}
+	
+	
+	void Update ()
+    {
+		
+	}
+}
---
+++ Assets/Ginger/Ginger/play.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using System;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class play : MonoBehaviour
+{
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("lobby", LoadSceneMode.Single);
+    }
+}
